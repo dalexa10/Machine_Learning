bf99b598781038c1a88a2ad515adf796650812b1
Dario <dario.rodriguez9378@hotmail.com>
2023-11-08 18:12:22 -0600

    code update to handle results from ffnn

--- 5_Scientific_Machine_Learning/5_PINNs_Euler_Equations/post_processing.py
+++ 5_Scientific_Machine_Learning/5_PINNs_Euler_Equations/post_processing.py
@@ -7,31 +7,61 @@ import matplotlib.pyplot as plt
 import forward_PINN
 from analytic_solutions import SodShockAnalytic
 
-# Import loss history
-loss_folder = 'loss_history/'
-files = ['loss_history.pkl', 'loss_history_w.pkl', 'loss_history_ext.pkl', 'loss_history_w_ext.pkl']
-for file in files:
-    with open(os.path.join(loss_folder, file), 'rb') as f:
-        loaded_file = pickle.load(f)
-        variable_name = file.split('.')[0]
-        exec(variable_name + ' = loaded_file')
+# Import outcomes from training
 
-# Load models
-model = forward_PINN.ffnn(30, 7)
-model.load_state_dict(torch.load('trained_models/model.pth'))
-model.eval()
+results_folder = 'results/'
+subfolders = [f.path for f in os.scandir(results_folder) if f.is_dir()]
 
-model_w = forward_PINN.ffnn(30, 7)
-model_w.load_state_dict(torch.load('trained_models/model_w.pth'))
-model_w.eval()
+models_dict = {}
 
-model_ext = forward_PINN.ffnn(30, 7)
-model_ext.load_state_dict(torch.load('trained_models/model_ext.pth'))
-model_ext.eval()
+for i, subfolder in enumerate(subfolders):
+    try:
+        # Load model data
+        with open(os.path.join(subfolder, 'model_data_dict.pkl'), 'rb') as f:
+            model_data_dict = pickle.load(f)
 
-model_w_ext = forward_PINN.ffnn(30, 7)
-model_w_ext.load_state_dict(torch.load('trained_models/model_w_ext.pth'))
-model_w_ext.eval()
+        # Load history training
+        with open(os.path.join(subfolder, 'loss_history.pkl'), 'rb') as f:
+            loss_history = pickle.load(f)
+
+        model = forward_PINN.ffnn(nn_width=model_data_dict['width'],
+                                  nn_hidden=model_data_dict['hidden'],
+                                  activation_type=model_data_dict['activation'])
+        model.load_state_dict(torch.load(os.path.join(subfolder, 'model.pth')))
+        model.eval()
+
+        models_dict[i] = {'model': model,
+                          'model_data_dict': model_data_dict,
+                          'loss_history': loss_history}
+    except FileNotFoundError:
+        print('FileNotFoundError')
+        continue
+
+
+
+# files = ['loss_history.pkl', 'loss_history_w.pkl', 'loss_history_ext.pkl', 'loss_history_w_ext.pkl']
+# for file in files:
+#     with open(os.path.join(loss_folder, file), 'rb') as f:
+#         loaded_file = pickle.load(f)
+#         variable_name = file.split('.')[0]
+#         exec(variable_name + ' = loaded_file')
+#
+# # Load models
+# model = forward_PINN.ffnn(30, 7)
+# model.load_state_dict(torch.load('trained_models/model.pth'))
+# model.eval()
+#
+# model_w = forward_PINN.ffnn(30, 7)
+# model_w.load_state_dict(torch.load('trained_models/model_w.pth'))
+# model_w.eval()
+#
+# model_ext = forward_PINN.ffnn(30, 7)
+# model_ext.load_state_dict(torch.load('trained_models/model_ext.pth'))
+# model_ext.eval()
+#
+# model_w_ext = forward_PINN.ffnn(30, 7)
+# model_w_ext.load_state_dict(torch.load('trained_models/model_w_ext.pth'))
+# model_w_ext.eval()
 
 # Compute analytic solutions
 x_exact = np.linspace(0, 1, 100)
@@ -50,64 +80,68 @@ tx_test = np.hstack((t_test, x_test))
 tx_test = torch.tensor(tx_test, dtype=torch.float32).to(device)
 
 with torch.no_grad():
-    # Compute predictions for plain model
-    U_pred = model(tx_test)
-    rho_pred = U_pred[:, 0].detach().cpu().numpy()
-    u_pred = U_pred[:, 1].detach().cpu().numpy()
-    p_pred = U_pred[:, 2].detach().cpu().numpy()
+    for k in models_dict.keys():
+        model = models_dict[k]['model']
+        U_pred = model(tx_test)
+        rho_pred = U_pred[:, 0].detach().cpu().numpy()
+        u_pred = U_pred[:, 1].detach().cpu().numpy()
+        p_pred = U_pred[:, 2].detach().cpu().numpy()
 
-    # Compute prediction for weighted model
-    U_pred_w = model_w(tx_test)
-    rho_pred_w = U_pred_w[:, 0].detach().cpu().numpy()
-    u_pred_w = U_pred_w[:, 1].detach().cpu().numpy()
-    p_pred_w = U_pred_w[:, 2].detach().cpu().numpy()
+        models_dict[k]['rho_pred'] = rho_pred
+        models_dict[k]['u_pred'] = u_pred
+        models_dict[k]['p_pred'] = p_pred
 
-    # Compute prediction for extended model
-    U_pred_ext = model_ext(tx_test)
-    rho_pred_ext = U_pred_ext[:, 0].detach().cpu().numpy()
-    u_pred_ext = U_pred_ext[:, 1].detach().cpu().numpy()
-    p_pred_ext = U_pred_ext[:, 2].detach().cpu().numpy()
 
-    # Compute prediction for weighted and extended model
-    U_pred_w_ext = model_w_ext(tx_test)
-    rho_pred_w_ext = U_pred_w_ext[:, 0].detach().cpu().numpy()
-    u_pred_w_ext = U_pred_w_ext[:, 1].detach().cpu().numpy()
-    p_pred_w_ext = U_pred_w_ext[:, 2].detach().cpu().numpy()
+    # # Compute predictions for plain model
+    # U_pred = model(tx_test)
+    # rho_pred = U_pred[:, 0].detach().cpu().numpy()
+    # u_pred = U_pred[:, 1].detach().cpu().numpy()
+    # p_pred = U_pred[:, 2].detach().cpu().numpy()
+    #
+    # # Compute prediction for weighted model
+    # U_pred_w = model_w(tx_test)
+    # rho_pred_w = U_pred_w[:, 0].detach().cpu().numpy()
+    # u_pred_w = U_pred_w[:, 1].detach().cpu().numpy()
+    # p_pred_w = U_pred_w[:, 2].detach().cpu().numpy()
+    #
+    # # Compute prediction for extended model
+    # U_pred_ext = model_ext(tx_test)
+    # rho_pred_ext = U_pred_ext[:, 0].detach().cpu().numpy()
+    # u_pred_ext = U_pred_ext[:, 1].detach().cpu().numpy()
+    # p_pred_ext = U_pred_ext[:, 2].detach().cpu().numpy()
+    #
+    # # Compute prediction for weighted and extended model
+    # U_pred_w_ext = model_w_ext(tx_test)
+    # rho_pred_w_ext = U_pred_w_ext[:, 0].detach().cpu().numpy()
+    # u_pred_w_ext = U_pred_w_ext[:, 1].detach().cpu().numpy()
+    # p_pred_w_ext = U_pred_w_ext[:, 2].detach().cpu().numpy()
 
 #%%
 fig, ax = plt.subplots(2,2 , figsize=(10, 8))
-ax[0, 0].plot(loss_history, label='PINN')
-ax[0, 0].plot(loss_history_w, label='PINN + W')
-ax[0, 0].plot(loss_history_ext, label='PINN + Ext')
-ax[0, 0].plot(loss_history_w_ext, label='PINN + W + Ext')
+for k in models_dict.keys():
+    ax[0, 0].plot(models_dict['loss_history'], label=models_dict['model_data_dict']['case_name'])
 ax[0, 0].set_xlabel('Epochs')
 ax[0, 0].set_yscale('log')
 ax[0, 0].set_ylabel('Loss')
 ax[0, 0].legend()
 
-ax[0, 1].plot(x_test.numpy(), rho_pred, label='PINN')
-ax[0, 1].plot(x_test.numpy(), rho_pred_w, label='PINN + W')
-ax[0, 1].plot(x_test.numpy(), rho_pred_ext, label='PINN + Ext')
-ax[0, 1].plot(x_test.numpy(), rho_pred_w_ext, label='PINN + W + Ext')
 ax[0, 1].plot(x_exact, rho_exact, 'k--', label='Analytic')
+for k in models_dict.keys():
+    ax[0, 1].plot(x_test.numpy(), models_dict[k]['rho_pred'], label=models_dict[k]['model_data_dict']['case_name'])
 ax[0, 1].set_xlabel(r'$x$')
 ax[0, 1].set_ylabel(r'$\rho$')
 ax[0, 1].legend()
 
-ax[1, 0].plot(x_test.numpy(), u_pred, label='PINN')
-ax[1, 0].plot(x_test.numpy(), u_pred_w, label='PINN + W')
-ax[1, 0].plot(x_test.numpy(), u_pred_ext, label='PINN + Ext')
-ax[1, 0].plot(x_test.numpy(), u_pred_w_ext, label='PINN + W + Ext')
 ax[1, 0].plot(x_exact, u_exact, 'k--', label='Analytic')
+for k in models_dict.keys():
+    ax[1, 0].plot(x_test.numpy(), models_dict[k]['u_pred'], label=models_dict[k]['model_data_dict']['case_name'])
 ax[1, 0].set_xlabel(r'$x$')
 ax[1, 0].set_ylabel(r'$u$')
 ax[1, 0].legend()
 
-ax[1, 1].plot(x_test.numpy(), p_pred, label='PINN')
-ax[1, 1].plot(x_test.numpy(), p_pred_w, label='PINN + W')
-ax[1, 1].plot(x_test.numpy(), p_pred_ext, label='PINN + Ext')
-ax[1, 1].plot(x_test.numpy(), p_pred_w_ext, label='PINN + W + Ext')
 ax[1, 1].plot(x_exact, p_exact, 'k--', label='Analytic')
+for k in models_dict.keys():
+    ax[1, 1].plot(x_test.numpy(), models_dict[k]['p_pred'], label=models_dict[k]['model_data_dict']['case_name'])
 ax[1, 1].set_xlabel(r'$x$')
 ax[1, 1].set_ylabel(r'$p$')
 ax[1, 1].legend()
