48063da2095ba55a152799f2c3f611a6974a3085
Dario <dario.rodriguez9378@hotmail.com>
2023-11-08 20:33:15 -0600

    decrease number of cases to be tested

--- 5_Scientific_Machine_Learning/5_PINNs_Euler_Equations/multiprocessing_training.py
+++ 5_Scientific_Machine_Learning/5_PINNs_Euler_Equations/multiprocessing_training.py
@@ -5,10 +5,10 @@ from forward_PINN import train_NN_Euler_equations
 # Parameters to be tested in the PINN
 ext_domain = [0, 1]
 weights = [0, 1]
-width = [20, 30, 40]
-hidden = [5, 7, 9]
-activation = ['tanh', 'relu', 'silu']
-lr = [0.0005, 0.001, 0.005]
+width = [20, 30]
+hidden = [7, 9]
+activation = ['tanh', 'silu']  # relu
+lr = [0.0005]
 
 iter_ls = list(itertools.product(ext_domain, weights, width, hidden, activation, lr))
 
